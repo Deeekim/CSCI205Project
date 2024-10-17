63b4bddee66982766287db05656cc06d045aab15
Deeekim <140359277+Deeekim@users.noreply.github.com>
2024-10-17 18:16:03 +0800

    Updated TileManager.java BUT MORE THAN JAVADOC
    added String param for loadMap( ) function which is the .txt file name

--- TileManager.java
+++ TileManager.java
@@ -3,24 +3,48 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+* TileManager
+*
+* Created By:
+* Name: Michael Deekimcheng, Miguel Yapan
+* ID: 205641, 205501
+* Date: October 6, 2024
+*
+* Description: This class is responsible for storing information on the game's world map,
+* including which tile is assigned to which x and y coordinates. The class also ensures that 
+* tiles are loaded correctly, specifically tiles that fit within the frame size. This helps reduce
+* issues in rendering too much game elements at once.
+*
+* @author Michael Deekimcheng, Miguel Yapan
+* @version 1.2
+*/
 public class TileManager {
 
-    private SceneCanvas gc;
+    private SceneCanvas sc;
     private DrawingObject[] tiles; // Array of tile objects
     private int[][] mapTileNum; // Store which tile to use at each position
 
-    public TileManager(SceneCanvas gc) {
-        this.gc = gc;
+    /**
+    * Constructs a TileManager for the given SceneCavas
+    *
+    * @param sc the SceneCanvas which the map will be drawn on
+    */
+    public TileManager(SceneCanvas sc) {
+        this.sc = sc;
         
         // Create an array of tiles
         tiles = new DrawingObject[10];
         loadTiles();
 
-        // Example map: create a 2D array representing a grid of tile types
-        mapTileNum = new int[gc.getMaxWorldCol()][gc.getMaxWorldRow()];
-        loadMap();
+        // Create a 2D array of integers for tiles to reference
+        mapTileNum = new int[sc.getMaxWorldCol()][sc.getMaxWorldRow()];
+        loadMap('map1.txt');
     }
 
+    /**
+    * Instantiates and assigns an integer to each possible tile type in the game
+    */
     public void loadTiles() {
 
         tiles[0] = new SandTile();  // Tile type 0: sand
@@ -32,21 +56,28 @@ public class TileManager {
 
     }
 
-    public void loadMap() {
+    /**
+    * Instantiates a map with tiles given an .txt input file of length 50 integers by 50 integers containing 
+    * white-spaced separated integers ranging from 0 to the number of tiles loaded. The number of integers
+    * depends on the maximum number of world columns and maximum number of world rows given by variables
+    * maxWorldCol and maxWorldRow, repsectively.
+    * 
+    * @param map_name the name of the .txt file storing integers
+    */
+    public void loadMap(String map_name) {
  
         try {
-            String map_name = "map1.txt";
             InputStream is = getClass().getResourceAsStream(map_name);
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
             int col = 0;
             int row = 0;
             
-            while(col < gc.getMaxWorldCol() && row < gc.getMaxWorldRow()) {
+            while(col < sc.getMaxWorldCol() && row < sc.getMaxWorldRow()) {
                 
                 String line = br.readLine();
 
-                while(col < gc.getMaxWorldCol()) {
+                while(col < sc.getMaxWorldCol()) {
 
                     String numbers[] = line.split(" ");
 
@@ -56,7 +87,7 @@ public class TileManager {
                     col++;
                     
                 }
-                if(col == gc.getMaxWorldCol()) {
+                if(col == sc.getMaxWorldCol()) {
                     col = 0;
                     row++;
                 }
@@ -68,24 +99,29 @@ public class TileManager {
         }
     }
 
+    /**
+    * Draws the tiles on the screen that fit within the player's screen
+    * 
+    * @param g2d the Graphics2D object used for drawing the tiles
+    */
     public void draw(Graphics2D g2d) {
         
         int worldCol = 0;
         int worldRow = 0;
 
-        while(worldCol < gc.getMaxWorldCol() && worldRow < gc.getMaxWorldRow()) {
+        while(worldCol < sc.getMaxWorldCol() && worldRow < sc.getMaxWorldRow()) {
             int tileNum = mapTileNum[worldRow][worldCol];
             DrawingObject tile = tiles[tileNum];
 
-            int worldX = worldCol * gc.getTileSize();
-            int worldY = worldRow * gc.getTileSize();
-            int screenX = worldX - gc.getPlayer().getWorldX() + gc.getPlayer().getScreenX();
-            int screenY = worldY - gc.getPlayer().getWorldY() + gc.getPlayer().getScreenY();
+            int worldX = worldCol * sc.getTileSize();
+            int worldY = worldRow * sc.getTileSize();
+            int screenX = worldX - sc.getPlayer().getWorldX() + sc.getPlayer().getScreenX();
+            int screenY = worldY - sc.getPlayer().getWorldY() + sc.getPlayer().getScreenY();
 
-            if (worldX > gc.getPlayer().getWorldX() - gc.getPlayer().getScreenX() - gc.getTileSize() && 
-                worldX < gc.getPlayer().getWorldX() + gc.getPlayer().getScreenX() + gc.getTileSize() &&
-                worldY > gc.getPlayer().getWorldY() - gc.getPlayer().getScreenY() - gc.getTileSize() && 
-                worldY < gc.getPlayer().getWorldY() + gc.getPlayer().getScreenY() + gc.getTileSize()) {
+            if (worldX > sc.getPlayer().getWorldX() - sc.getPlayer().getScreenX() - sc.getTileSize() && 
+                worldX < sc.getPlayer().getWorldX() + sc.getPlayer().getScreenX() + sc.getTileSize() &&
+                worldY > sc.getPlayer().getWorldY() - sc.getPlayer().getScreenY() - sc.getTileSize() && 
+                worldY < sc.getPlayer().getWorldY() + sc.getPlayer().getScreenY() + sc.getTileSize()) {
                         g2d.translate(screenX, screenY);
                         tile.drawTile(g2d);
                         g2d.translate(-screenX, -screenY);
@@ -94,7 +130,7 @@ public class TileManager {
             // Move the tile to its correct position and draw it
             worldCol++;
             
-            if (worldCol == gc.getMaxWorldCol()) {
+            if (worldCol == sc.getMaxWorldCol()) {
                 worldCol = 0;
                 worldRow++;
             }
@@ -102,10 +138,20 @@ public class TileManager {
     
     }
 
+    /**
+    * Returns the array of different tile types
+    *
+    * @return array of tiles
+    */
     public DrawingObject[] getTiles() {
         return tiles;
     }
 
+    /**
+    * Returns the 2D array of integers corresponding to tile number
+    *
+    * @return 2D array of integers 
+    */
     public int[][] getMapTileNum() {
         return mapTileNum;
     }
