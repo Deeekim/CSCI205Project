bbf70d7efdfac78656d14d25f09bd9622e9954e1
Deeekim <140359277+Deeekim@users.noreply.github.com>
2024-10-17 17:50:00 +0800

    Update SceneStarter.java with javadoc

--- SceneStarter.java
+++ SceneStarter.java
@@ -1,3 +1,17 @@
+/**
+* SceneStarter
+* 
+* Created by: 
+* Name: Michael Deekimcheng, Miguel Yapan 
+* ID: 205641, 205501
+* Date: September 29, 2024
+* 
+* Description: This class is responsible for instantiating the game scene and
+*              setting up the GUI elements.
+* 
+* @author Michael Deekimcheng, Miguel Yapan
+* @version 1.0
+*/
 
 public class SceneStarter {
 
