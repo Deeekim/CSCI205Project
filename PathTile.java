312edaa21a6a4b0b04a1dc3b7ecd5a392e6abcac
Miguel Yapan <69042459+mcyapan@users.noreply.github.com>
2024-10-19 00:06:36 +0800

    Update PathTile.java javadoc comments

--- PathTile.java
+++ PathTile.java
@@ -1,10 +1,28 @@
 import java.awt.*;
 
+/**
+ * PathTile
+ * 
+ * Created by:
+ * Name: Michael Deekimcheng, Miguel Yapan
+ * ID: 205641, 205501
+ * Date: September 29, 2024
+ * 
+ * Description: The PathTile is a composite shape DrawingObject subclass that is used to render the paths on the map.
+ * 
+ * @author Michael Deekimcheng, Miguel Yapan
+ */
 public class PathTile extends DrawingObject {
 
+    /**
+     * The dirtColor and particleColor variables are used as parameters to create the shape.
+     */
     private Color dirtColor = new Color(155, 118, 83);
     private Color particleColor = new Color(125, 98, 63);
 
+    /**
+     * Square and Circle classes are created to form a path shape that will be added to the list of tiles in the createTile method.
+     */
     private Square dirt = new Square(0, 0, 48, dirtColor, false);
     private Circle particle1 = new Circle(6, 5, 3, particleColor, false);
     private Circle particle2 = new Circle(24, 15, 3, particleColor, false);
@@ -16,11 +34,20 @@ public class PathTile extends DrawingObject {
     private Circle particle8 = new Circle(27, 35, 3, particleColor, false);
     private Circle particle9 = new Circle(35, 41, 3, particleColor, false);
 
+    /**
+     * Creates a PathTile object using the createTile method described below.
+     * Since the PathTile object represents the paths on the map, the Player object should not collide with this class.
+     * Thus, the collision parameter from the DrawingObject class is set to false.
+     */
     public PathTile() {
         super(false); // Assuming no collision for path
         createTile();
     }
 
+    /**
+     * Creates a tile by adding the shape and color of the instantiated Square and Circle objects to the array of tiles.
+     * The shapes and colors are added using the addTile method from the DrawingObject class.
+     */
     private void createTile() {
         
         addTile(dirt.getShape(), dirt.getColor());
