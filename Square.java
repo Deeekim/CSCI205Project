4e3031f3b0c19d3c03785989abfb050a50ee6869
Miguel Yapan <69042459+mcyapan@users.noreply.github.com>
2024-10-18 17:50:07 +0800

    Update javadoc comments

--- Square.java
+++ Square.java
@@ -1,11 +1,29 @@
 import java.awt.*;
 import java.awt.geom.*;
 
+/**
+ * Square
+ * 
+ * The Square class creates a square object given the user's input parameters.
+ * This class is used to create shapes to add design to the tiles.
+ * 
+ * @author Michael Deekimcheng
+ * @version 1.0
+ */
 public class Square extends DrawingObject{
     
     private Color color;
     private Rectangle2D.Double square;
-
+    
+    /**
+     * Creates a Square object with the user's specifications.
+     * 
+     * @param x the x-coordinate of the location where the square will be drawn 
+     * @param y the y-coordinate of the location where the square will be drawn
+     * @param size the length of a side of the square
+     * @param color the color of the square
+     * @param collision a boolean parameter which determines whether the player could collide with an object (true) or not (false)
+     */
     public Square(double x, double y, double size, Color color, boolean collision) {
 
         super(collision);
@@ -14,17 +32,20 @@ public class Square extends DrawingObject{
     
     }
 
-    public void draw(Graphics2D g2d) {
-        
-        g2d.setColor(color);
-        g2d.fill(square);
-
-    }
-
+    /**
+     * Getter method for the square's shape
+     * 
+     * @return the square's shape
+     */
     public Shape getShape() {
         return square;
     }
 
+    /**
+     * Getter method for the square's color
+     * 
+     * @return the square's color
+     */    
     public Color getColor() {
         return color;
     }
