dc1f408748921779087efa587ae300bea4654532
Miguel Yapan <69042459+mcyapan@users.noreply.github.com>
2024-10-18 17:29:33 +0800

    Added javadoc comments

--- Circle.java
+++ Circle.java
@@ -1,11 +1,34 @@
 import java.awt.*;
 import java.awt.geom.*;
 
+/**
+ * Circle
+ * 
+ * Created by: 
+ * Name: Michael Deekimcheng, Miguel Yapan
+ * ID: 205641, 205501
+ * Date: September 29, 2024
+ *
+ * Description: The Circle class creates a circle object given the user's input parameters.
+ * This class is used to create shapes to add design to the tiles.
+ * 
+ * @author Michael Deekimcheng, Miguel Yapan
+ * @version 1.0
+ */
 public class Circle extends DrawingObject {
     
     private Color color;
     private Ellipse2D.Double circle; 
 
+    /**
+     * Creates a Circle object with the user's specifications
+     * 
+     * @param x the x-coordinate of the location where the circle will be drawn 
+     * @param y the y-coordinate of the location where the circle will be drawn
+     * @param size the size of the circle
+     * @param color the color of the circle
+     * @param collision a boolean parameter which determines whether the player could collide with an object (true) or not (false)
+     */
     public Circle(double x, double y, double size, Color color, boolean collision) {
 
         super(collision);
@@ -13,15 +36,20 @@ public class Circle extends DrawingObject {
         this.circle = new Ellipse2D.Double(x, y, size, size);
     }
 
-    public void draw(Graphics2D g2d) {
-        g2d.setColor(color);
-        g2d.fill(circle); 
-    }
-
+    /**
+     * Getter method for the circle's shape
+     * 
+     * @return the circle's shape
+     */
     public Shape getShape() {
         return circle;
     }
 
+    /**
+     * Getter method for the circle's color
+     * 
+     * @return the circle's color
+     */    
     public Color getColor() {
         return color;
     }
