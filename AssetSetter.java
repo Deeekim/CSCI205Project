7af45a2e3a93de5b559a77259e0c713979573d61
Miguel Yapan <69042459+mcyapan@users.noreply.github.com>
2024-10-20 20:51:24 +0800

    Updated javadoc comments

--- AssetSetter.java
+++ AssetSetter.java
@@ -1,32 +1,35 @@
 /**
- * The class is responsible for initializing and setting 
- * various objects on the frame. In this project, it places 
- * `Dot` objects on the game scene at specified tiles.
+ * AssetSetter
+ * 
+ * Description: The class is responsible for initializing and setting various objects on the frame. 
+ * In this project, it places `Dot` objects on the game scene at specified tiles.
+ * Authors: Michael Deekimcheng, Miguel Yapan
+ * ID Number: 205641, 205501
+ * Date: September 29, 2024
  */
 public class AssetSetter {
     
-    /** The object used for drawing the tiles, objects, and entities */
-    private SceneCanvas gc;
+    private SceneCanvas sc;
 
     /**
-     * Constructs an {@code AssetSetter} with the specified {@code SceneCanvas}.
+     * Constructs an AssetSetter with the specified SceneCanvas sc.
      * 
-     * @param gc the {@code SceneCanvas} instance that this {@code AssetSetter} will manage
+     * @param gc the SceneCanvas instance that this AssetSetter will manage.
      */
-    public AssetSetter(SceneCanvas gc) {
-        this.gc = gc;
+    public AssetSetter(SceneCanvas sc) {
+        this.sc = sc;
     }
 
     /**
      * Sets the objects on the scene by placing `Dot` objects at predefined 
      * positions. These initial positions are calculated by multiplying the tile size  
      * to a coder-identified column and row indices.
+     * We can add more Dot objects by increasing the size of the object list.
      */
     public void setObject() {
-        // Add more dots here depending on size of obj list
-        gc.getObj()[0] = new Dot(25 * gc.getTileSize(), 23 * gc.getTileSize());
-        gc.getObj()[1] = new Dot(26 * gc.getTileSize(), 24 * gc.getTileSize());
-        gc.getObj()[2] = new Dot(26 * gc.getTileSize(), 22 * gc.getTileSize());
+        sc.getObj()[0] = new Dot(25 * sc.getTileSize(), 23 * sc.getTileSize());
+        sc.getObj()[1] = new Dot(26 * sc.getTileSize(), 24 * sc.getTileSize());
+        sc.getObj()[2] = new Dot(26 * sc.getTileSize(), 22 * sc.getTileSize());
     }
 
 }
