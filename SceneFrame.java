231eb44519f5605b097cadf86bf3dc3db5721adf
Miguel Yapan <69042459+mcyapan@users.noreply.github.com>
2024-10-20 22:45:54 +0800

    updated javadoc comments

--- SceneFrame.java
+++ SceneFrame.java
@@ -1,13 +1,24 @@
-// idea: put button to switch maps
-
 import javax.swing.*;
 
+/**
+ * SceneFrame
+ * 
+ * Description: This class is responsible for setting up the parameters of the animation.
+ * A JFrame will be instantiated and the visual elements will be added from the SceneCanvas class.
+ * Authors: Michael Deekimcheng, Miguel Yapan
+ * ID Number: 205641, 205501
+ * Date: September 29, 2024
+ */
 public class SceneFrame {
+
     private JFrame frame;
     private int width;
     private int height;
     private SceneCanvas sc;
 
+    /**
+     * Constructor instantiates a new SceneCanvas and SceneFrame.
+     */
     public SceneFrame() {
         frame = new JFrame();
         width = 800;
@@ -15,6 +26,10 @@ public class SceneFrame {
         sc = new SceneCanvas();
     }
 
+    /**
+     * Sets the parameters of the JFrame according to specifications. 
+     * The setUpGame and startGameThread method from the SceneCanvas are also called.
+     */ 
     public void setUpGUI() {
 
         frame.setResizable(false);
