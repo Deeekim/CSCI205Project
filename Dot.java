049e6f057b84ce283229be8a3814dc7ea7f0685e
Miguel Yapan <69042459+mcyapan@users.noreply.github.com>
2024-10-20 21:32:28 +0800

    updated javadoc comments

--- Dot.java
+++ Dot.java
@@ -1,9 +1,28 @@
 import java.awt.*;
 import java.awt.geom.*;
 
+/**
+ * Dot
+ *
+ * Description: The class creates dots that the Player can interact with, increasing their score.
+ * The dot class will first be instantiated at specific points. Once the player picks up these initial dots,
+ * additional dots will be generated randomly in the map.
+ * Authors: Michael Deekimcheng, Miguel Yapan
+ * ID Number: 205641, 205501
+ * Date: September 29, 2024
+ */
 public class Dot extends DrawingObject {
 
-    // Constructor to initialize a dot (circle) at a given world position
+    private Circle dotOuter = new Circle(12, 12, 24, Color.ORANGE, false);
+    private Circle dotMiddle = new Circle(15, 15, 18, Color.YELLOW, false);
+    private Circle dotInner = new Circle(24, 18, 6, Color.WHITE, false);
+
+    /** Creates a Dot object at a specified position in the world.
+     * Initial dots will also be created under the constructor
+     *
+     * @param worldX the x-coordinate position of the dot.
+     * @param worldY the y-coordinate position of the dot.
+     */
     public Dot(int worldX, int worldY) {
         super(false);
         this.setName("Dot");
@@ -15,13 +34,9 @@ public class Dot extends DrawingObject {
         this.setSolidAreaWidth(24);
         this.setSolidAreaHeight(24);
 
-        // // Initialize shapes and colors arrays
-        // this.shapes = new ArrayList<>();
-        // this.colors = new ArrayList<>();
-
         // Add shapes and colors for the Dot object
-        addShape(new Ellipse2D.Double(12, 12, 24, 24), Color.ORANGE);
-        addShape(new Ellipse2D.Double(15, 15, 18, 18), Color.YELLOW);
-        addShape(new Ellipse2D.Double(24, 18, 6, 6), Color.WHITE);
+        addShape(dotOuter.getShape(), dotOuter.getColor());
+        addShape(dotMiddle.getShape(), dotMiddle.getColor());
+        addShape(dotInner.getShape(), dotInner.getColor());
     }
 }
